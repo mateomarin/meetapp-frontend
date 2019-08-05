58cff583d215f5a2f5a40deb3ec30264d908ae4d
mateomarin <mateo.mw@gmail.com>
2019-08-05 17:15:43 -0300

    setup of all auth services, store and signin/signup pages

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/images/M.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signUpRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="meetapp logo" />
+      <Form onSubmit={handleSubmit} schema={schema}>
+        <Input name="name" type="text" placeholder="Seu nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha de acesso"
+        />
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Criar conta'}
+        </button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
+}
