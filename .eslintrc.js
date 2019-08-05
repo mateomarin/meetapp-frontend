f6d1bbddbc534f74742d36ef55b0981d81db1434
mateomarin <mateo.mw@gmail.com>
2019-08-05 15:45:18 -0300

    first eslint prettier clean setup

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks',
+    'jsx-a11y'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+      'react/jsx-filename-extension': [
+          'warn',
+          {extensions: ['.jsx', '.js']}
+      ],
+      'import/prefer-default-export': 'off',
+      "no-unused-vars": ["error", {argsIgnorePattern: "ˆ_"}],
+      "react/jsx-one-expression-per-line": "off",
+      "global-require": "off",
+      "react-native/no-raw-text": "off",
+      "no-param-reassign": "off",
+      "no-underscore-dangle": "off",
+      camelcase: "off",
+      "no-console": ["error", {allow: ["tron"]}],
+      'react-hooks/rules-of-hooks':  'error',
+      'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: 'src'
+      }
+    }
+  }
+};
